fd9a3e05c9b42ae9c2a1b67b344090b934e2fbf6
aaa <seominsunam03@gmail.com>
2026-09-09 17:52:12 +0900

    Update tesk 12-2.c

--- ch 12/tesk 12-2.c
+++ ch 12/tesk 12-2.c
@@ -43,3 +43,28 @@ int main(void)
 
     return 0;
 }
+// **********************************************
+// 제   목  :  실습과제5 포인터를 이용한 변수 값 변경 및 포인터 교환
+// 날   짜  :  2026년 9월 9일
+// 작성자   :  2600093
+// **********************************************
+#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 30, num2 = 50;
+    int* ptr1 = &num1;
+    int* ptr2 = &num2;
+    int* temp;
+
+    *ptr1 += 20;
+    *ptr2 -= 10;
+
+    temp = ptr1;
+    ptr1 = ptr2;
+    ptr2 = temp;
+
+    printf("ptr1이 가리키는 값 : %d\n", *ptr1);
+
+    return 0;
+}
