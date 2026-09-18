e1fa8e229e2838bd9fc8ca36eb4f3b837866c3d2
aaa <seominsunam03@gmail.com>
2026-09-18 14:03:45 +0900

    Update ch 13-2.c

--- ch 13/ch 13-2.c
+++ ch 13/ch 13-2.c
@@ -1,1 +1,23 @@
+// **********************************************
+// 제   목  :  실습과제2 문자열의 각 문자를 출력하는 프로그램
+// 날   짜  :  2026년 9월 18일
+// 작성자   :  2600093
+// **********************************************
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+#include<stdio.h>
 
+int main(void) {
+	char str[100];
+	int i = 0;
+
+	printf("문자열을 입력하시오:");
+	scanf("%s", str);
+
+	while (str[i] != '\0') {
+		printf("%d번째문자 %c\n", i + 1, str[i]);
+		i++;
+	}
+
+	return 0;
+}
