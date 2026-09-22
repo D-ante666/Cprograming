4509241e7b992e29f57720ac9de7f100a5fa7def
aaa <seominsunam03@gmail.com>
2026-09-23 01:52:28 +0900

    Update ch14-1.c

--- ch14/ch14-1.c
+++ ch14/ch14-1.c
@@ -48,4 +48,45 @@ int add2(int value)
     value += 2;
     return value;
 }
+```c
+// ******************************************************************************************
+// 제   목  :  실습과제4
+// 날   짜  :  2026년 9월 23일
+// 작성자   :  2600093
+// ******************************************************************************************
 
+#include <stdio.h>
+
+void swap(int* px, int* py);
+
+int main(void)
+{
+    int x, y, z;
+
+    printf("정수를 입력 하시오: ");
+    scanf("%d", &x);
+
+    printf("정수를 입력 하시오: ");
+    scanf("%d", &y);
+
+    printf("정수를 입력 하시오: ");
+    scanf("%d", &z);
+
+    printf("swap함수 호출 전 x=%d, y=%d, z=%d\n", x, y, z);
+
+    swap(&x, &y);
+    swap(&y, &z);
+
+    printf("swap함수 호출 후 x=%d, y=%d, z=%d\n", x, y, z);
+
+    return 0;
+}
+
+void swap(int* px, int* py)
+{
+    int tmp;
+
+    tmp = *px;
+    *px = *py;
+    *py = tmp;
+}
