9312be77dcd3f9f3c28c55e689137d7c8ee71bde
aaa <seominsunam03@gmail.com>
2026-09-23 01:47:39 +0900

    Update ch14-1.c

--- ch14/ch14-1.c
+++ ch14/ch14-1.c
@@ -18,3 +18,34 @@ void add2(int* value)
 {
     *value += 2;
 }
+
+// ******************************************************************************************
+// 제   목  :  실습과제3
+// 날   짜  :  2026년 9월 23일
+// 작성자   :  2600093
+// ******************************************************************************************
+
+#include <stdio.h>
+
+int add2(int value);
+
+int main(void)
+{
+    int number;
+
+    printf("정수를 입력하세요:");
+    scanf("%d", &number);
+
+    number = add2(number);
+
+    printf("2만큼 증가한 값:%d\n", number);
+
+    return 0;
+}
+
+int add2(int value)
+{
+    value += 2;
+    return value;
+}
+
